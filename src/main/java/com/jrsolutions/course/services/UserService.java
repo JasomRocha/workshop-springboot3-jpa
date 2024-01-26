caec619021a11c7213a8399f93b80371404975fd
Jasom Rocha <jasomrocha27@gmail.com>
2024-01-26 12:02:12 -0300

    Exception Handling update

--- src/main/java/com/jrsolutions/course/services/UserService.java
+++ src/main/java/com/jrsolutions/course/services/UserService.java
@@ -13,6 +13,8 @@ import com.jrsolutions.course.repositories.UserRepository;
 import com.jrsolutions.course.services.exceptions.DatabaseException;
 import com.jrsolutions.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service //informammos ao spring que essa classe é um componente do tipo servico
 public class UserService {
 	
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+	try {	
 		User entity = repository.getReferenceById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+	}catch(EntityNotFoundException e){
+		throw new ResourceNotFoundException(id);
+	}
 	}
 
 	private void updateData(User entity, User obj) {
